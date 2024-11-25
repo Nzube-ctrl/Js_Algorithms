3013bfae807f9f335587eab14611e089b794b433
Nzube Uwakwe <nzubeuwakwe@gmail.com>
2024-11-25 20:47:31 +0300

    Added string_pattern

---
+++ string_pattern.js
@@ -0,0 +1,15 @@
+function steps(n) {
+  for (let row = 1; row <= n; row++) {
+    let line = ``;
+    for (let col = 1; col <= n; col++) {
+      if (col <= row) {
+        line += `#`;
+      } else {
+        line += ` `;
+      }
+    }
+    console.log(line);
+  }
+}
+
+steps(5);
